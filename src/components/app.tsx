1e07177a15dd3e26d539a7e4cda58900a5a5279f
Zahra Mirkazemi <zahramirkazemi1997@gmail.com>
2024-08-30 02:20:42 +0330

    feat: Add todo-list component

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
-import { ConfigProvider } from "antd";
+import { useEffect, useState } from "react";
+import { ConfigProvider, Flex, List } from "antd";
 import { darkTheme, lightTheme } from "../constant";
 import "../style/app.css";
+import useTodoStore from "../store/todo";
+import TodoList from "./todo-list";
 
 const App = () => {
   const [isDarkMode, setDarkMode] = useState(true);
+  const { todoList, fetchTodoList } = useTodoStore();
+
+  useEffect(() => {
+    fetchTodoList();
+  }, []);
 
   return (
     <ConfigProvider
@@ -13,6 +20,10 @@ const App = () => {
       }}
     >
       <div className="content">
+        <Flex gap="middle" align="start" justify="space-around">
+          <TodoList listTitle='Todo' listItems={todoList.filter(todo => !todo.completed)} />
+          <TodoList listTitle='Done!' listItems={todoList.filter(todo => todo.completed)} />
+        </Flex>
       </div>
     </ConfigProvider>
   );
