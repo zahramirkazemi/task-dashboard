e8af99e229a5cec259bdd359d2a7c9b8bef532fe
Zahra Mirkazemi <zahramirkazemi1997@gmail.com>
2024-08-30 17:07:56 +0330

    feat: Modify themes color

--- src/constant/dark-theme.tsx
+++ src/constant/dark-theme.tsx
@@ -1,6 +1,11 @@
 const darkTheme = {
-  colorPrimary: '#EEEEEE',
+  colorPrimary: '#595e9b',
+  colorFillContent: '#EEEEEE',
+  colorTextBase: '#EEEEEE',
+  colorPrimaryBg: '#222831',
   colorBgContainer: '#222831',
+  colorText: '#EEEEEE',
+  colorIcon: '#222831',
 };
 
 export default darkTheme;
